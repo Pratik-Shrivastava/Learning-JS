57994f5ec6e9ff68a316a79e460f6cb85bc78b77
Pratik Shrivastava <108012653+Pratik-Shrivastava@users.noreply.github.com>
2024-01-08 06:08:34 +0000

    Learnt about primitive and non-primitive types

--- 01_basics/02_operation.js
+++ 01_basics/02_operation.js
@@ -1,8 +1,20 @@
-/*
-console.log(2 + 3)
-console.log(2 - 3)
-console.log(2 * 3)
-console.log(2 ** 3)
-console.log(2 / 3)
-console.log(2 % 3)
-*/
+
+// console.log(2 + 3)
+// console.log(2 - 3)
+// console.log(2 * 3)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2 % 3)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(false);
+// console.log(+true);
+// console.log(-true);
+// console.log(-false);
+// console.log(+false);
---
+++ 01_basics/04_summary.js
@@ -0,0 +1,11 @@
+//Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Refernce (Non-Primitive)
+
+// Array, Objects, Functions
+
+//JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
